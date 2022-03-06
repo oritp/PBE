5f26494ea23b7b380f5f63653e0c332663dd30c8
oritp <100279838+oritp@users.noreply.github.com>
2022-03-06 11:40:48 +0100

    Update puzzle1.rb

--- Puzzle1/puzzle1.rb
+++ Puzzle1/puzzle1.rb
@@ -1,12 +1,23 @@
+require 'ruby-nfc'
+
 class Rfid
-    # return uid in hexa str
-    def read_uid
-        ...
-    end
+        @@reader = NFC::Reader.all
+        def read_uid
+                @@reader[0].poll(Mifare::Classic::Tag) do |tag|
+                begin
+                        uid = uid_hex.upcase!
+                        #uid = tag.to_s.split()[0].upcase!
+                        return uid
+                rescue Exception => e
+                        puts e
+                end
+                end
+        end
 end
 
 if __FILE__ == $0
-    rf = Rfid...new
-    uid = rf.read_uid
-    puts uid
+        rf = Rfid. new
+        puts "Waiting for a card..."
+        uid = rf.read_uid
+        puts "Your user ID is: " + uid
 end
