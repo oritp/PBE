40c367c67e52dc067226fcfb9c1383ace376f295
oritp <100279838+oritp@users.noreply.github.com>
2022-05-03 23:22:05 +0200

    Create rfid.rb

---
+++ CDR/Client/rfid.rb
@@ -0,0 +1,10 @@
+require 'ruby-nfc'
+
+class Rfid
+    @@reader = NFC::Reader.all
+    def read_uid
+        @@reader[0].poll(Mifare::Classic::Tag) do |tag|
+	          return tag.uid_hex.upcase
+  	    end
+    end
+end
