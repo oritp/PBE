2e7f1c61441a41f8735435b46016e67fa31e104b
oritp <100279838+oritp@users.noreply.github.com>
2022-05-03 23:44:06 +0200

    Update timetableHandler.js

--- CDR/Server/Handlers/timetableHandler.js
+++ CDR/Server/Handlers/timetableHandler.js
@@ -1,13 +1,13 @@
 var url = require('url');
 var options = require('./options.js');
  
-var handler = (req, res) => {
+var handler = (req, res) => { 
     var params = new URLSearchParams(url.parse(req.url).query);
     var id = params.get('uid');
     sql = "SELECT * FROM timetables WHERE uid = '" + id + "'";
-    params.delete('uid');
+    params.delete('uid'); 
     
-	var limit = null;
+    var limit = null;
     if (params.has('limit')) {
 		limit = params.get('limit');
 		params.delete('limit');
@@ -17,15 +17,15 @@ var handler = (req, res) => {
     let date = new Date(now);
     var day = null, hour = null;
     
-    if(params.has('day') == false)
-		day = date.getDay();
-    if(params.has('hour') == false)
+    if (params.has('day') == false)
+    	day = date.getDay();
+    if (params.has('hour') == false)
 		hour = date.getUTCHours()+1+":00:00";
     if (params[0] == '[lt]')
 		params[0] = params[0].replace('[lt]', '<');
     else if (params[0] == '[gt]')
         params[0] = params[0].replace('[gt]', '>');
-    else if(params[0] = '[gte]')
+    else if (params[0] = '[gte]')
         params[0] = params[0].replace('[gte]','>');
     if (params[0] == 'day')
 		params[1] = options.dayNumb(params[1]);
@@ -39,18 +39,18 @@ var handler = (req, res) => {
         if (res != null) {
             var i = 0;
             while (i < result.length){
-                if(options.compareTime(result[i].hour) && day!=null && date.getDay()==result[i].day)
+                if (options.compareTime(result[i].hour) && day!=null && date.getDay()==result[i].day)
                     result.splice(i, 1);
                 else 
-					i++;
+		    i++;
             }
             i = 0;
-            while(i < result.length){
+            while (i < result.length){
                 result[i].day = options.dayName(result[i].day);
                 i++;
             }
-            if(limit != null) {
-				while(result.length > limit)
+            if (limit != null) {
+				while (result.length > limit)
 					result.pop();
 			}
         }
