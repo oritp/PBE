7fad80f8065fe5ba31298e7d7eb86da1dcb0b5e9
oritp <100279838+oritp@users.noreply.github.com>
2022-05-04 19:46:20 +0200

    Update rfid.rb

--- CDR/Client/rfid.rb
+++ CDR/Client/rfid.rb
@@ -4,7 +4,7 @@ class Rfid
     @@reader = NFC::Reader.all
     def read_uid
         @@reader[0].poll(Mifare::Classic::Tag) do |tag|
-	          return tag.uid_hex.upcase
-  	    end
+		return tag.uid_hex.upcase
+  	end
     end
 end
