1cbd5b7edfcfd0ba03f67bf85beadb3dc1f8f6a0
oritp <100279838+oritp@users.noreply.github.com>
2022-05-05 19:45:20 +0200

    Update lcd.rb

--- CDR/Client Ruby/lcd.rb
+++ CDR/Client Ruby/lcd.rb
@@ -1,25 +1,26 @@
 require "gtk3"
-require 'i2c/drivers/lcd'
+require "i2c/drivers/lcd"
 
 class LCD
 	def inicialitzar
 		@display=I2C::Drivers::LCD::Display.new('/dev/i2c-1',0x27)
 		@files=4
 		@columnes=20
-		
 		@display.clear
 		@display.text("Introduce your ID:", 1)
 	end
+	
 	def printNom(nom)
 		#Neteja LCD
 		@display.clear
 		#Printem a la LCD
-		@display.text('Welcome',1)
+		@display.text("Welcome",1)
 		@display.text(nom, 2)
 	end
+	
 	def printInvalid
 		@display.clear
-		@display.text('UID',1)
-		@display.text('INVALID',2)
+		@display.text("UID",1)
+		@display.text("INVALID",2)
 	end
 end
