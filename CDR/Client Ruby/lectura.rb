abe8ac08000fefd15a80af0fbdf54b6a9bade803
oritp <100279838+oritp@users.noreply.github.com>
2022-05-24 00:16:24 +0200

    Update lectura.rb

--- CDR/Client Ruby/lectura.rb
+++ CDR/Client Ruby/lectura.rb
@@ -2,42 +2,47 @@ require 'lcd.rb'
 require 'rfid.rb'
 require 'net/http'
 require 'json'
+require 'thread'
 
 #Lectura UID
 class Connexio
 	display=LCD.new
 	display.inicialitzar
-	
-	def llegirNom (uid) #Lectura del servidor, que ens retorna el nom del usuari
-		uri=URI("http://localhost:3000/students?uid="+uid) #buscar a la path:/students 
-		request=Net::HTTP::Get.new(uri)
-				
-		Net::HTTP.start(uri.host,uri.port) do |http| #creates conection to HTTP server
-			response=http.request(request)
-			data=JSON.parse(response.body) #El server ens retorna un arxiu JSON(response), que hem de parsejar
-			name=data["name"]
-			
+
+	def llegir(query, uid) #Lectura del servidor, que ens retorna el nom del usuari
+		Thread.new{
+			if query==null
+				uri=URI("http://localhost:3000/students?uid="+uid) #buscar a la path:/students
+			end
+			else
+				uri=URI("http://localhost:3000/"+query+"&uid="+uid)
+			end
+		
+			request=Net::HTTP::Get.new(uri)
+		
+			Net::HTTP.start(uri.host,uri.port) do |http| #creates conection to HTTP server
+				response=http.request(request)
+				data=JSON.parse(response.body) #El server ens retorna un arxiu JSON(response), que hem de parsejar
+			end
+
 			#Exemple execucio
 			#data=JSON.parse({"name":"Oriol Torres","uid":"ED467D29"})
-			#puts data["name"]  => "Oriol Torres"
-		end
+			#puts data["name"] =>"Oriol Torres"
 		
-		if name == "NotFound" #si el uid no està a la nostra BD
-			display.printInvalid
-		else  #si el uid sí està a la nostra BD
-			display.printNom(name)
-		end
-		return name
+			name=data["name"]
+			if name == "NotFound" #si el uid no esta a la nostra BD
+				display.printInvalid
+			else #si el uid si esta a la nostra BD
+				display.printNom(name)
+			end
+			
+			#---------RETURNS-----------
+			if query==null
+				return name
+			end
+			else
+				return data
+			end
+		}
 	end
-	
-	def llegirQuery(query, uid)
-		uri=URI("http://localhost:3000/"+query+"&uid="+uid)
-		request=Net::HTTP::Get.new(uri)
-		
-		Net::HTTP.start(uri.host ,uri.port) do |http| #creates conection to HTTP server
-			response=http.request(request)
-			info=JSON.parse(response.body) #El server ens retorna un arxiu JSON(response), que hem de parsejar
-		end
-		return info
-	end	
-end 
+end
